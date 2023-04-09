7e1c104c1a0105779042363b41ea9035b27da16b
TTC-CCF <93956496+TTC-CCF@users.noreply.github.com>
2023-04-10 02:29:06 +0800

    Add openai

--- liff.js
+++ liff.js
@@ -13,25 +13,29 @@ async function showProfile(){
 }
 
 async function getRecommand(){
-    const mt = $("input[name='MealsType']").value;
-    const cal = $("input[name='Carolies']").value;
-    const { Configuration, OpenAIApi } = require("openai");
-
-    const configuration = new Configuration({
-      apiKey: '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',
+    await liff.sendMessages({
+        type:'text',
+        text:'你好啊'
     });
-    const openai = new OpenAIApi(configuration);
+    // const mt = $("input[name='MealsType']").value;
+    // const cal = $("input[name='Carolies']").value;
+    // const { Configuration, OpenAIApi } = require("openai");
+
+    // const configuration = new Configuration({
+    //   apiKey: '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',
+    // });
+    // const openai = new OpenAIApi(configuration);
     
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "我想吃"+mt+"，熱量不能超過"+String(cal)+"，請給我一些推薦的菜色。格式:菜名 熱量(卡)。",
-      temperature: 0.5,
-      max_tokens: 100,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
+    // const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: "我想吃"+mt+"，熱量不能超過"+String(cal)+"，請給我一些推薦的菜色。格式:菜名 熱量(卡)。",
+    //   temperature: 0.5,
+    //   max_tokens: 100,
+    //   frequency_penalty: 0.0,
+    //   presence_penalty: 0.0,
+    // });
 
-    console.log(response);
+    // console.log(response);
 }
 
 initialize()
