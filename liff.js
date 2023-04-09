32df1b2860c47458f4db8ca74bc032f45eab2f83
TTC-CCF <93956496+TTC-CCF@users.noreply.github.com>
2023-04-10 02:03:58 +0800

    Add openai

--- liff.js
+++ liff.js
@@ -12,6 +12,26 @@ async function showProfile(){
     $('#user').text('Hello '+profile.displayName);
 }
 
+async function getRecommand(){
+    const mt = $("input[name='MealsType']").value;
+    const cal = $("input[name='Carolies']").value;
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+      apiKey: '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',
+    });
+    const openai = new OpenAIApi(configuration);
+    
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "我想吃"+mt+"，熱量不能超過"+String(cal)+"，請給我一些推薦的菜色。格式:菜名 熱量(卡)。",
+      temperature: 0.5,
+      max_tokens: 100,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+}
+
 initialize()
     .then(()=>{
         showProfile();
