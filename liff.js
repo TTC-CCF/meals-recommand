837f8f465908f16cb2789fb465be1ae0c3f9f023
TTC-CCF <93956496+TTC-CCF@users.noreply.github.com>
2023-04-13 04:00:40 +0800

    Add get api

--- liff.js
+++ liff.js
@@ -17,14 +17,13 @@ async function getRecommand(){
   const url ="https://api.openai.com/v1/chat/completions"
   const mt = $("[name='MealsType']").val();
   const cal = $("[name='Calories']").val();
-  console.log("我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，你有什麼推薦的餐點嗎? 格式: '菜名' '熱量'卡");
   fetch(url, {
     method: 'POST',
     body: JSON.stringify({
-      "model":"gpt-3.5-turbo",
+      "model":"text-davinci-003",
       "messages": [{
         "role":"user", 
-        "content": "我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，你有什麼推薦的餐點嗎? 格式: '菜名' '熱量'卡",
+        "content": "我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，請列出一些菜名，格式: '菜名' '熱量'卡",
       }],
       "temperature":0.5
     }),
