85629923164fecc28e2fb3eefdb638bf9e8a427c
TTC-CCF <93956496+TTC-CCF@users.noreply.github.com>
2023-04-13 17:15:34 +0800

    Feat: prettify openai response

--- liff.js
+++ liff.js
@@ -17,12 +17,14 @@ async function getRecommand(){
   const url ="https://api.openai.com/v1/completions"
   const mt = $("[name='MealsType']").val();
   const cal = $("[name='Calories']").val();
+  $("#user").empty();
+  $("#notloading").attr("id","loading");
   fetch(url, {
     method: 'POST',
     body: JSON.stringify({
       "model":"text-davinci-003",
       "max_tokens": 1000,
-      "prompt": "我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，請列出一些菜名，格式: '菜名' '熱量'卡",
+      "prompt": "我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，請列出一些菜名，格式: '菜名' '熱量'大卡。",
       "temperature":0.5
     }),
     headers:{
@@ -33,6 +35,13 @@ async function getRecommand(){
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    var contents = String(data.choices['0'].text);
+    contents = contents.replace(/\n/g, '<br>');
+    contents = contents.replace(/'/g, '');
+    $("#loading").attr("id","notloading");
+    $("#user").append(
+      $("<article class='message is-dark'><div class='message-header'><p>"+mt+"<br></p></div><div class='message-body'>"+contents+"</div></article>")
+    );
   })
   .catch((error) => {
     console.error('Error:', error);
