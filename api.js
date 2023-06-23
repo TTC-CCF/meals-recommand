0e080d95160d06a7e0ff13e808fe33b86883c0e4
TTC-CCF <93956496+TTC-CCF@users.noreply.github.com>
2023-06-23 18:32:17 +0800

    Init: first push

---
+++ api.js
@@ -0,0 +1,81 @@
+require('dotenv').config()
+const openai_key = process.env.OPENAI_KEY
+const express = require('express')
+const app = express()
+const port = 3000
+const url = "https://api.openai.com/v1/completions"
+
+app.post('/apis/get_recommend',  (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:10')
+    mt = req["mt"]
+    cal = req["cal"]
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          "model":"text-davinci-003",
+          "max_tokens": 300,
+          "prompt": "我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，請列出一些菜名，格式: '菜名' '熱量'大卡。",
+          "temperature":0.5
+        }),
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization':'Bearer '+ openai_key,
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+          var contents = String(data.choices['0'].text);
+          contents = contents.replace(/\n/g, '<br>');
+          contents = contents.replace(/'/g, '');
+          res.send(contents)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        return null
+      });    
+
+    // console.log(recommend)
+    // res.send(recommend)
+})
+
+app.options('/apis/get_recommend', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:10')
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.sendStatus(204)
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
+
+async function getRecommand(mt, cal){
+    const url ="https://api.openai.com/v1/completions"
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        "model":"text-davinci-003",
+        "max_tokens": 300,
+        "prompt": "我想要吃"+String(mt)+"，熱量不能超過"+String(cal)+"，請列出一些菜名，格式: '菜名' '熱量'大卡。",
+        "temperature":0.5
+      }),
+      headers:{
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+ openai_key,
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        var contents = String(data.choices['0'].text);
+        contents = contents.replace(/\n/g, '<br>');
+        contents = contents.replace(/'/g, '');
+        return contents
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      return null
+    });
+    
+  }
